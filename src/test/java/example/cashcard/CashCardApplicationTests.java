07a41c2d68a452efd62764b791b19fe350a9e86f
IceMajor <icemajorr2@gmail.com>
2023-06-06 15:56:49 +0200

    Comment out the 'webEnvironment' parameter inside @SpringBootTest annotation

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest//(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
 
 	// @Autowired should be used mainly in tests, not in source code
