3b411faa25dd26b96543d8910c27a91dadc5184e
IceMajor <icemajorr2@gmail.com>
2023-06-07 14:08:49 +0200

    Update CashCardRepository. Include owner argument in queries

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -8,5 +10,7 @@ import java.util.Optional;
 public interface CashCardRepository extends CrudRepository<CashCard, Long>,
         PagingAndSortingRepository<CashCard, Long> {
 
-    Optional<CashCard> findById(Long id);
+    Optional<CashCard> findByIdAndOwner(Long id, String owner); // returns a concrete card of a given owner
+
+    Page<CashCard> findByOwner(String owner, PageRequest amount); // returns all cards (in a list) of a given owner
 }
